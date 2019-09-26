729e831c2514e83a74d292415d7a80f43eef22d6
Camila Moura <camilapaleo@gmail.com>
2019-09-26 17:05:28 +0200

    Adding Dropdowns

--- ui-lib-project/src/index.js
+++ ui-lib-project/src/index.js
@@ -1,6 +1,14 @@
-import Tooltip from './project-ui/tooltip'
+import Tooltip from './project-ui/tooltip';
+import Dropdown from './project-ui/dropdown';
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
-
 tooltip.init();
+
+// create a dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+})
