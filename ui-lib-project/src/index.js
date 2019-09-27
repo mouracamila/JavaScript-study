a29e5242445522c2022b25bc38bd49ac77cc8933
Camila Moura <camilapaleo@gmail.com>
2019-09-27 14:37:36 +0200

    Snackbars

--- ui-lib-project/src/index.js
+++ ui-lib-project/src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from './project-ui/tooltip';
 import Dropdown from './project-ui/dropdown';
 import Tabs from './project-ui/tabs';
+import Snackbar from './project-ui/snackbar';
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -17,3 +18,12 @@ dropdowns.forEach(dropdown => {
 // create tabs
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
+
+// create snackbar
+const snackbar = new Snackbar(document.querySelector('.snackbar'));
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    snackbar.show('you clicked me :)');
+});
